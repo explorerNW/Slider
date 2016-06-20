13ac9c97d74a8263a554f5298a62b92b19e94503
iaku <iaku.li@qq.com>
2016-06-20 16:48:31 +0800

    v1.4 Update code to ES6

--- forjs/slider.js
+++ forjs/slider.js
@@ -1,12 +1,12 @@
-!function() {
+!(() => {
     // 将HTML转换为节点
-    function html2node(str) {
+    let html2node = (str) => {
         let container = document.createElement('div');
         container.innerHTML = str;
         return container.children[0];
     }
     // 帮助函数：赋值、扩展
-    function extend(a,b) {
+    let extend = (a,b) => {
         for (let i in b) {
             if (typeof a[i] === 'undefined') {
                 a[i] = b[i];
@@ -15,21 +15,21 @@
         return a;
     }
     // 帮助函数：add ClassName
-    function addClass(node, className) {
+    let addClass = (node, className) => {
         let current = node.className || "";
-        if ((" " + current + " ").indexOf(" " + className + " ") === -1) {
-        node.className = current? ( current + " " + className ) : className;
+        if ((` ${current} `).indexOf(` ${className} `) === -1) {
+        node.className = current? ( `${current} ${className}` ) : className;
         }
     }   
     // 帮助函数：remove ClassName
-    function removeClass(node, className){
+    let removeClass = (node, className) => {
         let current = node.className || "";
-        node.className = (" " + current + " ").replace(" " + className + " ", " ").trim();
+        node.className = (` ${current} `).replace(` ${className} `, " ").trim();
     }
     // 帮助函数：has ClassName
-    function hasClass(node,className) {
+    let hasClass = (node,className) => {
         let current = node.className || "";
-        if (current.match(new RegExp('(\\s|^)'+className+'(\\s|$)'))) {
+        if (current.match(new RegExp(`(\\s|^)${className}(\\s|$)`))) {
             return true;
         } else {
             return false;
@@ -51,10 +51,10 @@
         this.container.style.overflow = 'hidden';
         // 组件节点
         this.slider = this._layout.cloneNode(true);
-        this.slides = [].slice.call(this.slider.querySelectorAll('.slide'));
-        this.slidesIntro = [].slice.call(this.slider.querySelectorAll('.slide-intro'));
+        this.slides = Array.from(this.slider.querySelectorAll('.slide'));
+        this.slidesIntro = Array.from(this.slider.querySelectorAll('.slide-intro'));
         this.ctrlCon = this.container.querySelector('.m-slider-ctrl-con');
-        this.cons = [].slice.call(this.container.querySelectorAll('.slider-ctrl-cons'));    
+        this.cons = Array.from(this.container.querySelectorAll('.slider-ctrl-cons'));    
         // 初始化
         this.pageNum = this.images.length; 
         this.pageIndex = this.pageIndex || 0;
@@ -66,17 +66,17 @@
     extend(Slider.prototype,{
         _layout: html2node(template),
         // 初始设置 默认显示的图片
-        _init: function() {
-            this._step(0);       
+        _init() {
+            this._step(0); 
         },
-        prev: function() {
+        prev() {
             this._step(-1);
         },
-        next: function() {
+        next() {
             this._step(1);
         },
         // 图片切换逻辑控制
-        _step: function(offset) {
+        _step(offset) {
             this.pageIndex += offset;
             // 直接定位
             if (this.ctrlCon) {
@@ -91,12 +91,12 @@
             } 
             this.slider.style.transitionDuration = '.5s';
             // 偏移量
-            this.slider.style.transform = 'translateX(' + (-this.pageIndex*100) + '%)';
-            this.slides[this.slideIndex].style.left = (this.pageIndex*100) + '%';
-            this.slides[(this.slideIndex + 4)%3].style.left = ((this.pageIndex + 1)*100) + '%';
-            this.slides[(this.slideIndex + 2)%3].style.left = ((this.pageIndex - 1)*100) + '%';
+            this.slider.style.transform = `translateX(${-this.pageIndex*100}%)`;
+            this.slides[this.slideIndex].style.left = `${this.pageIndex*100}%`;
+            this.slides[(this.slideIndex + 4)%3].style.left = `${(this.pageIndex + 1)*100}%`;
+            this.slides[(this.slideIndex + 2)%3].style.left = `${(this.pageIndex - 1)*100}%`;
             // 设置图片(background)url
-            this.slides[this.slideIndex].style.background ='url(' + this.images[((this.pageIndex % this.pageNum) + this.pageNum) % this.pageNum] + ')' + '0 0 / 100% 100%' + 'no-repeat';
+            this.slides[this.slideIndex].style.background =`url(${this.images[((this.pageIndex % this.pageNum) + this.pageNum) % this.pageNum]})0 0 / 100% 100%no-repeat`;
             // 设置图片说明文字
             if (this.imgIntro) {
                 this.slidesIntro[this.slideIndex].style.display = 'block';
@@ -104,12 +104,12 @@
             }
         },
         // 直接定位
-        _nav: function() {
+        _nav() {
             this.ctrlCon.style.display = 'block';
             let that = this;
-            this.cons.forEach(function(con, index) {
+            this.cons.forEach((con, index) => {
                 // 点击con直接切换到目标slide
-                con.addEventListener('click', function() {
+                con.addEventListener('click', () => {
                     that.pageIndex = index;
                     that._step(0);
                 },false);
@@ -125,4 +125,4 @@
     
     // 暴露API到全局
     window.Slider = Slider;
-}()
+})()
